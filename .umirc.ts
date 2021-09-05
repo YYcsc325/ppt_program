c39f00960584ab28f1555600145946a0be8d8e19
yycsc <m13625744973@163.com>
2021-09-05 16:32:52 +0800

    refactor: 整理项目结构

---
+++ .umirc.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'umi';
+
+const env: any = process.env.NODE_ENV;
+
+export default defineConfig({
+  title: 'ppt_program',
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  history: {
+    type: 'hash',
+  },
+  publicPath:
+    env === 'development' ? './' : 'https://yycsc325.github.io/ppt_program/', // 部署到github上改变script的资源引入路径,
+  outputPath: 'build', // umi打包默认打成dist文件， gh-pages构建部署默认路径build
+  routes: [{ path: '/', component: '@/pages/index' }],
+
+  /** 全局less变量 */
+  theme: {
+    'border-color': '#eee',
+    'transition-delay': '.2s',
+    'light-gray': '#f9f9f9',
+  },
+  fastRefresh: {},
+});
