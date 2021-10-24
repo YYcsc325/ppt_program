56613e09acd23e2708277a06da7d04b6de1cd63d
yycsc <m13625744973@163.com>
2021-10-24 22:26:18 +0800

    refactor: oprator

--- src/pages/Editor/EditorContent/Canvas/EditableElement/index.tsx
+++ src/pages/Editor/EditorContent/Canvas/EditableElement/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useModel } from 'umi';
+import tinycolor from 'tinycolor2';
+import useGetter from '@/hooks/useGetter';
+import { utils } from 'react-dtcomponents';
+import SvgWrapper from '@/components/SvgWrapper';
+import { VIEWPORT_SIZE } from '@/config/canvas';
+
+import styles from './index.less';
+
+const editableElementPrefixCls = utils.createPrefixCls(
+  'editable-element',
+  styles,
+  'ppt',
+);
 
 const EditableElement = () => {
-  return <div>EditableElement</div>;
+  return <div className={editableElementPrefixCls()}>EditableElement</div>;
 };
 
 export default EditableElement;
--- src/pages/Editor/EditorContent/Canvas/MouseSelection/index.tsx
+++ src/pages/Editor/EditorContent/Canvas/MouseSelection/index.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import classNames from 'classnames';
+import { utils } from 'react-dtcomponents';
 
-const MouseSelection = () => {
-  return <div>MouseSelection</div>;
+import styles from './index.less';
+
+const mouseSelectionPrefixCls = utils.createPrefixCls(
+  'mouse-selection',
+  styles,
+  'ppt',
+);
+
+export interface MouseSelectionProps {
+  quadrant: number;
+  top: number;
+  left: number;
+  width: number;
+  height: number;
+}
+
+const MouseSelection: React.FC<MouseSelectionProps> = ({
+  quadrant,
+  top,
+  left,
+  width,
+  height,
+}) => {
+  return (
+    <div
+      className={classNames(
+        mouseSelectionPrefixCls(),
+        mouseSelectionPrefixCls(`uadrant-${quadrant}`),
+      )}
+      style={{
+        top: `${top}px`,
+        left: `${left}px`,
+        width: `${width}px`,
+        height: `${height}px`,
+      }}
+    ></div>
+  );
 };
 
 export default MouseSelection;
