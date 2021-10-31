0af1433fa11a9952dc958add4e6f5cac67de9ed8
yycsc <m13625744973@163.com>
2021-10-31 18:45:17 +0800

    refactor: shapeelement

--- src/pages/components/element/ShapeElement/index.tsx
+++ src/pages/components/element/ShapeElement/index.tsx
@@ -1,7 +1,53 @@
 import React from 'react';
+import { useModel } from 'umi';
+import classNames from 'classnames';
+import { utils } from 'react-dtcomponents';
+import SvgWrapper from '@/components/SvgWrapper';
+import { PPTShapeElement, ShapeText } from '@/types/slides';
+import { ContextmenuItem } from '@/components/Contextmenu/types';
 
-const ShapeElement = () => {
-  return <div>ShapeElement</div>;
+import styles from './index.less';
+
+const prefixCls = utils.createPrefixCls(
+  'editable-element-shape',
+  styles,
+  'ppt',
+);
+
+export interface ShapeElementProps {
+  className?: string;
+  elementInfo: PPTShapeElement;
+  selectElement: (
+    e: React.MouseEvent,
+    element: PPTShapeElement,
+    canMove?: boolean,
+  ) => void;
+  contextmenus: () => ContextmenuItem[];
+}
+
+const ShapeElement: React.FC<ShapeElementProps> = (props) => {
+  const { className, elementInfo } = props;
+  return (
+    <div
+      className={classNames(prefixCls(), className)}
+      style={{
+        top: elementInfo.top + 'px',
+        left: elementInfo.left + 'px',
+        width: elementInfo.width + 'px',
+        height: elementInfo.height + 'px',
+      }}
+    >
+      <div
+        className={prefixCls('rotate-wrapper')}
+        style={{ transform: `rotate(${elementInfo.rotate}deg)` }}
+      >
+        <div className={prefixCls('element-content')}>
+          <SvgWrapper></SvgWrapper>
+          <div className={prefixCls('shape-text')}></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ShapeElement;
